90c19e1cd95a04fc58c562668fb2f8ed261a2a66
KuntaePark <sgvelcro@naver.com>
2025-06-25 17:46:39 +0900

    init

---
+++ src/main/java/com/engjoy/EngjoyApplication.java
@@ -0,0 +1,13 @@
+package com.engjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngjoyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngjoyApplication.class, args);
+    }
+
+}
