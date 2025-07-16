4fc14fcee5cd4979409c29624b2b5bc4c7873b99
KuntaePark <sgvelcro@naver.com>
2025-07-16 12:49:18 +0900

    profile & logout

--- Web/src/main/java/com/engjoy/control/BingdingResult.java
+++
@@ -1,4 +0,0 @@
-package com.engjoy.control;
-
-public class BingdingResult {
-}
---
+++ Web/src/main/java/com/engjoy/security/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package com.engjoy.security;
+
+import com.engjoy.entity.Account;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+public class CustomUserDetails implements UserDetails {
+    private static final long serialVersionUID = 1L;
+    public String email;
+    public String name;
+    public String nickname;
+    public String password;
+
+    public CustomUserDetails(Account account) {
+        this.email = account.getEmail();
+        this.name = account.getName();
+        this.nickname = account.getNickname();
+        this.password = account.getPassword();
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {return password;}
+
+    @Override
+    public String getUsername() {return email;}
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
